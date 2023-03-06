0ab72baf0aee1c61815674fe403006f330f4f135
JorgePeju <pejuco@gmail.com>
2023-03-06 08:05:04 +0100

    aunqueda

--- js/main.js
+++ js/main.js
@@ -1,55 +1,106 @@
-//////// CONSTANTES ////////
+document.addEventListener("DOMContentLoaded", () => {
+
+  //            VARIABLES  - CONSTANTES           //
+
+  const arrayProductosSeleccionados = JSON.parse(localStorage.getItem('productos')) || [];
+
+  const urlIndex = "index.html"
+  const urlCarrito = "carritoCompra.HTML"
+
+  const estrellas = ["material_practica-carrito-compra\star1.png" , "material_practica-carrito-compra\star2.png"];
+
+  const divIndex = document.querySelector("#productos");
+  const divCesta = document.querySelector("#tablaCarrito");
+
+
+
+ //                  EVENTOS                      //
+
+ document.addEventListener('click', (ev) => {
+                          // quizá y probablemente {target}
+
+  // cambiar de index a carrito
+
+
+  // cambiar de carrito a Index
+
+
+  // actualizar carrito quizá dos
+
+
+  // 
+
+
+  const pintarEstrellitas = () => {
+
+  };
+
+  const setLocal = () => {
+
+    localStorage.setItem('productos', JSON.stringify(arrayProductosSeleccionados));
+
+};
+
+
+
+const getLocal = () => {
+
+    return JSON.parse(localStorage.getItem('productos')) || [];
+
+}; 
+
+
+
+
+ })
+
+  const consultarDatos = async (id) => {
+    let enlace;
+    if (id) {
+      enlace = `https://dummyjson.com/products/${id}`
+    } else {
+      enlace = 'https://dummyjson.com/products/'
+    }
+
+
+    try {
+      let solicitud = await fetch(enlace);
+      if (solicitud) {
+        let peticion = await solicitud.json()
+        return peticion
+      } else {
+        throw ({
+          ok: false,
+          mensaje: 'No pueden obtenerse los datos'
+        })
+      }
+    } catch (error) {
+      return error
+    }
+  }
+
+
+
+  const init = async () => {
+
+    let html = location.search
+    
+    if (html == urlIndex) {
+       // funcion pintar index
+       // funcion pintar carrito
+    };
+
+    if (html == urlCarrito) {
+       // obtener URL
+      // pintar carrito
+    }
+       
+    
+}
+init();
 
 
 
 
 
-// const pintarTabla = () => {
-//     tabla.innerHTML = "";
-//     .forEach(()=>{
-//         const producto = document.createElement("tr");
-//         producto.innerHTML += `<td>${producto.thumbnail}</td><td>${producto.nombre}</td><td>${producto.precio}</td><td>${producto.cantidad}</td><td>${producto.subtotal}</td>`;
-//         tabla.appendChild();
-//     })}
-
-
-
-
-
-    const consulta = async (busqueda, id) => {
-
-        try {
-    
-          let enlace;
-    
-          if  (busqueda != null) {
-    
-            enlace = `https://dummyjson.com/products/search?q=${busqueda}`;
-    
-          } else if (id != null) {
-    
-            enlace = `https://dummyjson.com/products`;
-    
-          }
-    
-          let peticion = await fetch(enlace,
-            {
-              method: "GET",
-            });
-    
-          if (peticion.ok) {
-            const respuesta = await peticion.json();
-            return respuesta;
-    
-          } else throw "Error en la petición";
-    
-        } catch (error) {
-    
-          return error;
-        }
-      };
-
-
-console.log(peticion.ok)
-    
-    
+});
