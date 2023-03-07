21df01b315b19c4c1e4b2c8c7f7c256bc06516ba
JorgePeju <pejuco@gmail.com>
2023-03-07 10:08:14 +0100

    cambios pintar

--- js/main.js
+++ js/main.js
@@ -1,58 +1,39 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
 
-  //            VARIABLES  - CONSTANTES           //
+
+  //              VARIABLES            //
+
 
   const arrayProductosSeleccionados = JSON.parse(localStorage.getItem('productos')) || [];
-
-  const urlIndex = "index.html"
-  const urlCarrito = "carritoCompra.HTML"
-
-  const estrellas = ["material_practica-carrito-compra\star1.png" , "material_practica-carrito-compra\star2.png"];
-
-  const divIndex = document.querySelector("#productos");
-  const divCesta = document.querySelector("#tablaCarrito");
-
-
-
- //                  EVENTOS                      //
-
- document.addEventListener('click', (ev) => {
-                          // quizá y probablemente {target}
-
-  // cambiar de index a carrito
-
-
-  // cambiar de carrito a Index
-
-
-  // actualizar carrito quizá dos
-
-
-  // 
-
-
-  const pintarEstrellitas = () => {
-
-  };
-
-  const setLocal = () => {
-
-    localStorage.setItem('productos', JSON.stringify(arrayProductosSeleccionados));
-
-};
-
-
-
-const getLocal = () => {
-
-    return JSON.parse(localStorage.getItem('productos')) || [];
-
-}; 
+  const divIndex = document.querySelector('#productos');
+  const pintarTablaCarrito = document.querySelector('#tablaCarrito');
+  
 
 
 
 
- })
+  //                EVENTOS               //
+
+  document.addEventListener('click', ({target}) => {
+
+      if(target.matches('#comprar')){
+          location.href = 'carritoCompra.html';
+      };
+
+      // if(target.matches('#volver')){ // cuando cree el boton volver
+      //     location.href = 'index.html';
+      // };
+
+      if(target.matches('#carrito')){
+          tablaCarrito.classList.toggle('hidden')
+      };
+
+
+  });
+
+
+
+  //                FUNCIONES                     //
 
   const consultarDatos = async (id) => {
     let enlace;
@@ -81,6 +62,132 @@ const getLocal = () => {
 
 
 
+  const pintarEstrellitas = () => {
+
+  
+
+  };
+
+
+
+  
+
+
+
+  const setLocal = () => {
+
+      localStorage.setItem('productos', JSON.stringify(arrayProductosSeleccionados));
+
+  }; 
+
+
+
+  const getLocal = () => {
+
+      return JSON.parse(localStorage.getItem('productos')) || [];
+
+  };
+
+
+
+  const almacenarDatos = async (id) => {   // revisar
+
+      const {solicitud} = await consultarDatos();
+
+      const {products} = solicitud;
+
+      let producto = products.find((item) => item.id == id);
+
+      if(arrayProductosSeleccionados.find((item) => item == producto)){
+
+
+      } else {
+          
+          let objProductosTabla = {
+              id: producto.id,
+              foto: producto.thumbnail,
+              nombre: producto.title,
+              precio: producto.price,
+              rating: producto.rating,
+              cantidad: 1,
+              subtotal: producto.price
+          }
+          
+          arrayProductosSeleccionados.push(objProductosTabla);
+
+          setLocal();
+
+          pintarTabla(id);
+
+      }
+
+  }; 
+
+  
+  const pintarIndex = async () => {
+    const {ok, solicitud} = await consultarDatos();
+    const {products} = solicitud;
+  
+    if (ok) {
+      let cards = '';
+  
+      products.forEach((item) => {
+        const imgEstrellas = pintarEstrellas(item.rating);
+  
+        const cardHTML = `
+          <article class="grid-item-cards">
+            <img src="${item.images[0]}">
+            <h3>${item.title}</h3>
+            <p>Precio: ${item.price.toLocaleString('de-DE')} €</p>
+            ${imgEstrellas}
+            <button class="card-btn" data-id="${item.id}">Añadir al carrito</button>
+          </article>
+        `;
+  
+        cards += cardHTML;
+      });
+  
+      divIndex.innerHTML = cards;
+    }
+  };
+
+
+  const pintarTabla = (id) => {
+    pintarTablaCarrito.innerHTML = '';
+  
+    const productos = getLocal();
+    const producto = productos.find((item) => item.id == id);
+  
+    if (producto) {
+      console.log('Producto encontrado. ');
+    } else {
+      let tablaHTML = '';
+  
+      productos.forEach((item) => {
+        const subtotal = item.precio * item.cantidad;
+  
+        const fila = `
+          <tr>
+            <td><img src="${item.foto}"></td>
+            <td>${item.nombre}</td>
+            <td>${item.precio.toLocaleString('de-DE')} €</td>
+            <td><i class="fa-sharp fa-solid fa-circle-minus"></i></td>
+            <td>${item.cantidad}</td>
+            <td><i class="fa-sharp fa-solid fa-circle-plus"></i></td>
+            <td>${subtotal.toLocaleString('de-DE')} €</td>
+            <td><i class="fa-sharp fa-solid fa-circle-xmark" id="xmark" data-id="${item.id}"></i></td>
+          </tr>
+        `;
+  
+        tablaHTML += fila;
+      });
+  
+      pintarTablaCarrito.innerHTML = tablaHTML;
+    }
+  };
+
+
+
   const init = async () => {
 
     let html = location.search
@@ -98,9 +205,6 @@ const getLocal = () => {
     
 }
 init();
-
-
-
 
 
 });
