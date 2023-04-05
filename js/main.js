9781bc103facf8f182e36d6a0bae2731e098c8fd
JorgePeju <pejuco@gmail.com>
2023-04-05 16:08:48 +0200

    todas funcionalidades completadas, falta maquetar

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   //              VARIABLES                 //
 
-
   const divCard = document.querySelector("#card");
   const divTabla = document.querySelector("#tabla");
   const toggle = document.querySelector(".toggle");
   const fragment = document.createDocumentFragment();
   const totalCarrito = document.getElementById("totalcarrito");
-
 
   const arrayProductosSeleccionados = JSON.parse(localStorage.getItem("productos")) || [];
 
@@ -17,28 +15,38 @@ document.addEventListener("DOMContentLoaded", () => {
   document.addEventListener("click", ({ target }) => {
 
     if (target.matches(".addBtn")) {
+
       let id = target.id;
       buscarProducto(id);
       pintarTabla()
+
     }
 
     if (target.matches(".comprar")) {
+
       location.href = "carritoCompra.html";
+
     }
 
     if (target.matches("#volver")) {
+
       location.href = "index.html";
+
     }
 
     if (target.matches(".carrito i")) {
+
       toggle.classList.toggle("ocultar")
+
     }
 
     if (target.matches(".vaciar")) {
+
       arrayProductosSeleccionados.length = 0;
       localStorage.removeItem("productos");
       totalCarrito.textContent = arrayProductosSeleccionados.length;
       pintarTabla();
+
     }
     
   });
@@ -47,7 +55,9 @@ document.addEventListener("DOMContentLoaded", () => {
   //                       FUNCIONES                   //
 
   const obtenerDatos = async () => {
+
     try {
+
       let ruta = `https://dummyjson.com/products/`;
 
       let peticion = await fetch(ruta, {
@@ -55,6 +65,7 @@ document.addEventListener("DOMContentLoaded", () => {
       });
 
       if (peticion.ok) {
+        
         const respuesta = await peticion.json();
 
         return respuesta;
@@ -68,37 +79,49 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   const setLocal = () => {
+
     localStorage.setItem("productos", JSON.stringify(arrayProductosSeleccionados));
+
   };
+
   const getLocal = () => {
+
     return arrayProductosSeleccionados;
+
   };
 
 
   const pintarIndex = async () => {
+
     let objProductos = await obtenerDatos();
     const arrayProductos = objProductos.products;
 
     innerHTML = "";
 
-    arrayProductos.forEach(({ title, id, images,estrellita }) => {
+    arrayProductos.forEach(({ title, id, images, rating }) => {
+
       const divCardIndex = document.createElement("DIV");
+      let estrellita = pintarEstrellas(rating);
       divCardIndex.classList.add("cardIndex");
       divCardIndex.innerHTML += `
           <div><img src="${images[0]}" class="card-img"></div>
-          <h2 class="card-title">${title}</h2> 
+          <h2 class="card-title">${title}</h2>
           <button class="addBtn" id="${id}">Añadir</button>`;
-          // Estrellitas(estrellita)
-      fragment.append(divCardIndex); // revisar el H del titulo
+
+      fragment.append(divCardIndex, estrellita);
+
     });
+
     divCard.append(fragment);
+
   };
 
-
   const pintarTabla = async () => {
+
     divTabla.innerHTML = "";
     const arrayProductosTabla = getLocal();
     let tablaHTML = "";
+
     arrayProductosTabla.forEach(({ titulo, precio, thumbnail, cantidad, subtotal }) => {
       tablaHTML += `<tr>
           <td class=""><div><img src="${thumbnail}" class="thumbnail"></div></td>
@@ -108,25 +131,29 @@ document.addEventListener("DOMContentLoaded", () => {
           <td>${subtotal}</td>
         </tr>`;
     });
+
     let comprarHTML = '<button class="comprar">Comprar</button>';
     let vaciarHTML = '<button class="vaciar">Vaciar</button>';
+
     tablaHTML += `<tr>
         <td colspan="4"></td>
         <td>${comprarHTML} ${vaciarHTML}</td>
       </tr>`;
     divTabla.innerHTML = tablaHTML;
+
   };
 
   const buscarProducto = async (id) => {
+
     const { products } = await obtenerDatos();
     const producto = products.find((item) => item.id == id)
-
 
     let cantidad = 1;
   
     let subtotal = cantidad * producto.price;
 
     if (producto) {
+
       const productoExiste = arrayProductosSeleccionados.find((item) => item.id == id)
 
       if (productoExiste) {
@@ -147,29 +174,52 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         arrayProductosSeleccionados.push(objNuevo);
+
       }
+
       totalCarrito.textContent = arrayProductosSeleccionados.length;
+
     }
+
     setLocal();
 
   };
 
-  // const Estrellitas = () => {
+  const pintarEstrellas = (rating) => {
 
+    const divEstrella = document.createElement("DIV");
+    divEstrella.className = "divEstrella";
+    
+    const estrellaVacia = "estrellas/star0.png";
+    const estrellaLlena = "estrellas/star1.png";
+    
+    for (let i = 1; i <= 5; i++) {
 
-  // }
+      const estrella = document.createElement("IMG");
+      estrella.src = i <= Math.round(rating) ? estrellaLlena : estrellaVacia;
+      divEstrella.append(estrella);
+
+    }
+    
+    return divEstrella;
+    
+  };
 
 
   const init = () => {
 
-    const url = location.toString(); // .href -> escritura/lectura
+    const url = location.toString();
 
     if (url.includes("carritoCompra")) {
+
       pintarTabla();
       getLocal();
+
     } else {
+
       pintarIndex();
       pintarTabla();
+
     }
   }
 
